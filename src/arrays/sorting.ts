2c7fd22058121c9ad67f6973d43232a09a27036a
Jyotindra Tavanoji <tavanojijyotindra@gmail.com>
2025-01-02 19:09:59 +0530

    Feature: Arrays reverse

--- src/arrays/sorting.ts
+++ src/arrays/sorting.ts
@@ -30,21 +30,34 @@
 // console.log(selectionSort([5,4,2,1,7]));
 
 // Merge Sort
-function mergeSort(arr: number[]): number[] {
-    if(arr.length <=1 ) return arr;
-    const mid = Math.floor(arr.length / 2);
-    const left = mergeSort(arr.slice(0, mid));
-    const right = mergeSort(arr.slice(mid));
-    return merge(left, right)
-}
+// function mergeSort(arr: number[]): number[] {
+//     if(arr.length <=1 ) return arr;
+//     const mid = Math.floor(arr.length / 2);
+//     const left = mergeSort(arr.slice(0, mid));
+//     const right = mergeSort(arr.slice(mid));
+//     return merge(left, right)
+// }
 
-function merge(left: number[], right: number[]): number[] {
-    let result: number[] = [], i=0, j=0;
-    while (i < left.length && j < right.length) {
-        if(left[i] < right[j]) result.push(left[i++]);
-        else result.push(right[j++])
+// function merge(left: number[], right: number[]): number[] {
+//     let result: number[] = [], i=0, j=0;
+//     while (i < left.length && j < right.length) {
+//         if(left[i] < right[j]) result.push(left[i++]);
+//         else result.push(right[j++])
+//     }
+//     return result.concat(left.slice(i)).concat(right.slice(j))
+// }
+
+// console.log(mergeSort([5, 3, 8, 4, 2])); // Output: [2, 3, 4, 5, 8]
+
+// Reverse an array
+function reverseArray(arr: number[]): number[] {
+    let left = 0, right = arr.length - 1;
+    while(left < right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
     }
-    return result.concat(left.slice(i)).concat(right.slice(j))
+    return arr
 }
 
-console.log(mergeSort([5, 3, 8, 4, 2])); // Output: [2, 3, 4, 5, 8]
+console.log(reverseArray([5,6,7,8,9,10]));
